7ea64ba03eda35b31ffa9c2fbe32d98e109fda25
Alberto Mengali <disberd@gmail.com>
2021-10-04 10:35:02 +0200

    re-implement notebook exclusive by file comments in CM6

--- frontend/components/Cell.js
+++ frontend/components/Cell.js
@@ -21,7 +21,7 @@ import { PlutoContext } from "../common/PlutoContext.js"
  * }} props
  * */
 export const Cell = ({
-    cell_input: { cell_id, code, code_folded, running_disabled },
+    cell_input: { cell_id, code, code_folded, running_disabled, notebook_exclusive },
     cell_result: { queued, running, runtime, errored, output, published_objects, depends_on_disabled_cells },
     cell_dependencies,
     cell_input_local,
@@ -115,6 +115,7 @@ export const Cell = ({
                 code_folded: class_code_folded,
                 running_disabled: running_disabled,
                 depends_on_disabled_cells: depends_on_disabled_cells,
+                notebook_exclusive: notebook_exclusive,
                 show_input: show_input,
                 drop_target: drag_active,
                 saving_file: saving_file,
@@ -187,6 +188,7 @@ export const Cell = ({
                 cell_id=${cell_id}
                 notebook_id=${notebook_id}
                 running_disabled=${running_disabled}
+                notebook_exclusive=${notebook_exclusive}
             />
             <${RunArea}
                 cell_id=${cell_id}
